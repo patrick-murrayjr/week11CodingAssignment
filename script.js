3f779b93741c784290d3ac0b08f22cbead365a17
Patrick Murray <patrick.murrayjr@gmail.com>
2023-06-07 17:45:17 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let GameOver = false;
+let playerOneTurn = true;
+let gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+//initialize game
+
+//#region MAIN GAME LOOP
+
+while (false) {
+  // if player one turn
+  //player one selects a square
+}
+//#endregion MAIN GAME LOOP
